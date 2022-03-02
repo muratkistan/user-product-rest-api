7b88cce2d16e634536f43c33d42bfb38834c461e
muratkistan <muratkistan@gmail.com>
2022-03-02 13:29:38 +0300

    Created Product Repository

---
+++ user-product-service/src/main/java/com/muratkistan/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.muratkistan.repository;
+
+import com.muratkistan.model.Product;
+import com.muratkistan.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByUser(User user);
+}
